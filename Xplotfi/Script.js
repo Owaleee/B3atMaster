009cb4e596ed736faf0bb7d239008bcafe3d0ad2
Owaleee <125622961+Owaleee@users.noreply.github.com>
2023-02-25 14:08:36 -0300

    Criação de visualização do projeto XplotFI
    Fonte de arquivos do projeto

---
+++ Xplotfi/Script.js
@@ -0,0 +1,85 @@
+const songname = document.getElementById("songname");
+const bandname = document.getElementById("bandname")
+const song = document.getElementById("audio");
+const cover = document.getElementById("cover")
+const play = document.getElementById("play")
+const next = document.getElementById("next")
+const back = document.getElementById("back")
+
+const napista = {
+    songname: "Na pista",
+    artist: "Thehc Mc,Aka Far",
+    file: "Na pista",
+};
+const Lucidez = {
+    songname: "Lucidez",
+    artist: "Thehc Mc",
+    file: "Lucidez",
+};
+const Minhalevada = {
+    songname: "Minha levada",
+    artist: "Thehc Mc",
+    file: "Minha levada",
+};
+
+let IsPlaying = false;
+const playlist = [napista, Lucidez, Minhalevada];
+let index = 0;
+
+
+function playsong() {
+    play.querySelector('.bi').classList.remove('bi-play-circle')
+    play.querySelector('.bi').classList.add('bi-pause-circle')
+    song.play();
+    IsPlaying = true
+}
+function pausesong() {
+    play.querySelector('.bi').classList.add('bi-play-circle')
+    play.querySelector('.bi').classList.remove('bi-pause-circle')
+    song.pause();
+    IsPlaying = false
+}
+function playPausedecider() {
+    if (IsPlaying === true) {
+        pausesong();
+    }
+    else {
+        playsong();
+    }
+}
+
+function inicializarmusica() {
+    cover.src = `./imagens/${playlist[index].file}.jpg`;
+    song.src = `./songs/${playlist[index].file}.mp3`;
+    songname.innerText = playlist[index].songname;
+    bandname.innerText = playlist[index].artist;
+}
+
+function backsong() {
+    if (index === 0) {
+        index = playlist.length - 1;
+    }
+    else {
+        index -= 1;
+    }
+    inicializarmusica();
+    playsong();
+}
+
+function nextsong() {
+    if (index === playlist.length - 1) {
+        index = 0;
+    }
+    else {
+        index += 1;
+    }
+    inicializarmusica();
+    playsong();
+}
+
+inicializarmusica();
+
+play.addEventListener('click', playPausedecider);
+back.addEventListener("click",backsong)
+next.addEventListener("click",nextsong)
+
